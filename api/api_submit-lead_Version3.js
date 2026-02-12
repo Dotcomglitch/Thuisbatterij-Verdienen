f63d84b1c5dcf4aa9d299733eec6c34149c35ff5
Dotcomglitch <v.hoogland88@gmail.com>
2026-02-12 14:55:39 +0100

    Delete api/api_submit-lead_Version3.js

--- api/api_submit-lead_Version3.js
+++
@@ -1,7 +0,0 @@
-// At the top of the file, replace these lines:
-const DATABOWL_PUBLIC_KEY = process.env.DATABOWL_PUBLIC_KEY || 'e485a81d2b79056fe7d46b24e95c3cf8';
-const DATABOWL_PRIVATE_KEY = process.env.DATABOWL_PRIVATE_KEY || 'db01497200b397e8a4f14ad78699a07a';
-
-// Update the submission URL with your Campaign ID and Supplier ID:
-const submissionUrl = `https://api.databowl.com/v1/leads/${campaignId}/${supplierId}/submit`;
-// Or use the endpoint from your Integration Document
