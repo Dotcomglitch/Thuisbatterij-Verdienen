e98cf78c31944fcf33de022797f87cc9be9ea49d
Dotcomglitch <v.hoogland88@gmail.com>
2026-02-12 14:46:33 +0100

    Delete api/api_validate_Version3.js

--- api/api_validate_Version3.js
+++
@@ -1,6 +0,0 @@
-// At the top of the file, replace these lines:
-const DATABOWL_API_URL = 'https://vapi.databowl.com/api/v1/validation';
-const DATABOWL_PUBLIC_KEY = process.env.DATABOWL_PUBLIC_KEY || 'e485a81d2b79056fe7d46b24e95c3cf8';
-const DATABOWL_PRIVATE_KEY = process.env.DATABOWL_PRIVATE_KEY || 'db01497200b397e8a4f14ad78699a07a';
-
-// They will automatically read from your environment variables!
