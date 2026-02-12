9903eb63c04f7efb4e0a1268e696cdeb09a11aea
Dotcomglitch <v.hoogland88@gmail.com>
2026-02-12 14:26:21 +0100

    Update api_submit-lead_Version2.js

--- api_submit-lead_Version2.js
+++ api_submit-lead_Version2.js
@@ -4,8 +4,8 @@
 const https = require('https');
 
 // DataBowl Configuration
-const DATABOWL_PUBLIC_KEY = process.env.DATABOWL_PUBLIC_KEY || 'YOUR_PUBLIC_KEY';
-const DATABOWL_PRIVATE_KEY = process.env.DATABOWL_PRIVATE_KEY || 'YOUR_PRIVATE_KEY';
+const DATABOWL_PUBLIC_KEY = process.env.DATABOWL_PUBLIC_KEY || 'e485a81d2b79056fe7d46b24e95c3cf8';
+const DATABOWL_PRIVATE_KEY = process.env.DATABOWL_PRIVATE_KEY || 'db01497200b397e8a4f14ad78699a07a';
 
 /**
  * Submit lead to DataBowl via HTTP POST
